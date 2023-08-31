27c171caea3542b042b2f1a13b81620550479f50
joaocerqueira <jbraga.cerq@gmail.com>
2023-08-31 08:57:10 -0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dotNetMVCSample.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
